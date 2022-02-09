70f7cd8d716e6a3f61ee79097fd24b62efc76129
josuedilmo <josuedilmo@gmail.com>
2022-02-09 18:45:31 +0000

    Question 6

--- Question6/LawnMowing.cs
+++ Question6/LawnMowing.cs
@@ -8,36 +8,33 @@ namespace ExercisesOOP.Question6
 {
     public class LawnMowing
     {
-        public void getLotSize(double lotMeterSquare)
+        public void getLotSize(double yardSquareMeter)
         {
             decimal lawnFee;
             const double week = 20;
-            if (lotMeterSquare < 400)
+            if (yardSquareMeter < 400)
             {
                 lawnFee = (decimal)week * 25;
-                string answer = $"The weekly price is $25 and 20-week season price is: {lawnFee}";
-                Console.WriteLine(answer.ToString());
+                Console.WriteLine($"The weekly price is $25 and 20-week season price is: {lawnFee}");
             }
-            else if (lotMeterSquare >= 400 || lotMeterSquare < 600)
+            else if (yardSquareMeter >= 400 || yardSquareMeter < 600)
             {
                 lawnFee = (decimal)week * 35;
-                string answer = $"The weekly price is $35 and 20-week season price is: {lawnFee}"; 
-                Console.WriteLine(answer.ToString());
+                Console.WriteLine($"The weekly price is $35 and 20-week season price is: {lawnFee}"); 
             }
-            else if (lotMeterSquare >= 600)
+            else if (yardSquareMeter >= 600)
             {
                 lawnFee = (decimal)week * 50;
-                string answer = $"The weekly price is $50 and 20-week season price is: {lawnFee}";
-                Console.WriteLine(answer.ToString());
+                Console.WriteLine($"The weekly price is $50 and 20 - week season price is: { lawnFee}");
             }
         }
 
         public void lawnMowing()
         {
             Console.WriteLine("Lawn-Mowing App");
-            Console.Write("Please enter the length of a lawn: ");
+            Console.Write("Please enter the length of your yard: ");
             bool validNumberOne = double.TryParse(Console.ReadLine(), out double lotLength);
-            Console.Write("Please enter the width of a lawn: ");
+            Console.Write("Please enter the width of your yard: ");
             bool validNumberTwo = double.TryParse(Console.ReadLine(), out double lotWidth);
             bool executeAct = validNumberOne && validNumberTwo;
             if (!executeAct)
@@ -47,8 +44,8 @@ namespace ExercisesOOP.Question6
             }
             else
             {
-                double lotMeterSquare = lotLength * lotWidth;
-                getLotSize(lotMeterSquare);
+                double yardSquareMeter = lotLength * lotWidth;
+                getLotSize(yardSquareMeter);
             }
         }
     }
